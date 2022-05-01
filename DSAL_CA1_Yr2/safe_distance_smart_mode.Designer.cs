9176d6b7698c373741ffc8918c42ddfe2b0e02b6
Vernell <vernell.lim2014@gmail.com>
2022-05-01 21:03:48 +0800

    safe_distance_smart_mode

---
+++ DSAL_CA1_Yr2/safe_distance_smart_mode.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace DSAL_CA1_Yr2
+{
+    partial class safe_distance_smart_mode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.layout = new System.Windows.Forms.Label();
+            this.btnA = new System.Windows.Forms.Button();
+            this.labelComment = new System.Windows.Forms.Label();
+            this.btnResetSimulation = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnSeatLayout = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.numRow = new System.Windows.Forms.NumericUpDown();
+            this.numCol = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numRow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCol)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layout
+            // 
+            this.layout.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.layout.Location = new System.Drawing.Point(336, 37);
+            this.layout.Name = "layout";
+            this.layout.Size = new System.Drawing.Size(1012, 1009);
+            this.layout.TabIndex = 63;
+            // 
+            // btnA
+            // 
+            this.btnA.BackColor = System.Drawing.Color.Yellow;
+            this.btnA.Location = new System.Drawing.Point(43, 213);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(270, 29);
+            this.btnA.TabIndex = 56;
+            this.btnA.Text = "Person A Booking";
+            this.btnA.UseVisualStyleBackColor = false;
+            // 
+            // labelComment
+            // 
+            this.labelComment.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelComment.Location = new System.Drawing.Point(43, 454);
+            this.labelComment.Name = "labelComment";
+            this.labelComment.Size = new System.Drawing.Size(270, 155);
+            this.labelComment.TabIndex = 62;
+            // 
+            // btnResetSimulation
+            // 
+            this.btnResetSimulation.Location = new System.Drawing.Point(43, 404);
+            this.btnResetSimulation.Name = "btnResetSimulation";
+            this.btnResetSimulation.Size = new System.Drawing.Size(270, 29);
+            this.btnResetSimulation.TabIndex = 60;
+            this.btnResetSimulation.Text = "Reset Simulation";
+            this.btnResetSimulation.UseVisualStyleBackColor = true;
+            // 
+            // btnD
+            // 
+            this.btnD.BackColor = System.Drawing.Color.Green;
+            this.btnD.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnD.Location = new System.Drawing.Point(43, 356);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(270, 29);
+            this.btnD.TabIndex = 59;
+            this.btnD.Text = "Person D Booking";
+            this.btnD.UseVisualStyleBackColor = false;
+            // 
+            // btnC
+            // 
+            this.btnC.BackColor = System.Drawing.Color.Red;
+            this.btnC.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnC.Location = new System.Drawing.Point(43, 306);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(270, 29);
+            this.btnC.TabIndex = 58;
+            this.btnC.Text = "Person C Booking";
+            this.btnC.UseVisualStyleBackColor = false;
+            // 
+            // btnB
+            // 
+            this.btnB.BackColor = System.Drawing.Color.Blue;
+            this.btnB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnB.Location = new System.Drawing.Point(43, 259);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(270, 29);
+            this.btnB.TabIndex = 57;
+            this.btnB.Text = "Person B Booking";
+            this.btnB.UseVisualStyleBackColor = false;
+            // 
+            // btnSeatLayout
+            // 
+            this.btnSeatLayout.Location = new System.Drawing.Point(43, 165);
+            this.btnSeatLayout.Name = "btnSeatLayout";
+            this.btnSeatLayout.Size = new System.Drawing.Size(270, 29);
+            this.btnSeatLayout.TabIndex = 53;
+            this.btnSeatLayout.Text = "Setup Cinema Seat Layout";
+            this.btnSeatLayout.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 46;
+            this.label2.Text = "Column";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 20);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "Row";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(201, 37);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(112, 29);
+            this.btnSave.TabIndex = 44;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(52, 37);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(108, 29);
+            this.btnLoad.TabIndex = 43;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // numRow
+            // 
+            this.numRow.Location = new System.Drawing.Point(163, 87);
+            this.numRow.Name = "numRow";
+            this.numRow.Size = new System.Drawing.Size(150, 27);
+            this.numRow.TabIndex = 64;
+            // 
+            // numCol
+            // 
+            this.numCol.Location = new System.Drawing.Point(163, 120);
+            this.numCol.Name = "numCol";
+            this.numCol.Size = new System.Drawing.Size(150, 27);
+            this.numCol.TabIndex = 65;
+            // 
+            // safe_distance_smart_mode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1373, 1055);
+            this.Controls.Add(this.numCol);
+            this.Controls.Add(this.numRow);
+            this.Controls.Add(this.layout);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.labelComment);
+            this.Controls.Add(this.btnResetSimulation);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnSeatLayout);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "safe_distance_smart_mode";
+            this.Text = "safe_distance_smart_mode";
+            ((System.ComponentModel.ISupportInitialize)(this.numRow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCol)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label layout;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Label labelComment;
+        private System.Windows.Forms.Button btnResetSimulation;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnSeatLayout;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.NumericUpDown numRow;
+        private System.Windows.Forms.NumericUpDown numCol;
+    }
+}
