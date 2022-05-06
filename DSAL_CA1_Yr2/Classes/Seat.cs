7087cf4dd0571b55ab5d75f7b6af17cd43460d12
Vernell <vernell.lim2014@gmail.com>
2022-05-06 20:07:49 +0800

    Create Seat class

---
+++ DSAL_CA1_Yr2/Classes/Seat.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSAL_CA1_Yr2.Classes
+{
+    class Seat
+    {
+        //Whether the seat has been selected
+        private bool _bookStatus = false;
+        //Seat is bookable or not
+        private bool _canBook = false;
+        //the _row field is 2 if obj modelling a seat at row "B"
+        private int _row;
+        //the _column field is 3 if obj modelling a seat at col 3
+        private int _column;
+
+        public int Row
+        {
+            get { return _row; }//get method
+            set { _row = value; }//set method
+        }//end of property Row
+
+        public int Column
+        {
+            get { return _column; }//get method
+            set { _column = value; }//set method
+        }//end of property Column
+
+        public bool CanBook
+        {
+            get { return _canBook; }//get method
+            set { _canBook = value; }//set method
+        }//end of property CanBook
+
+        public bool BookStatus
+        {
+            get { return _bookStatus; }//get method
+            set { _bookStatus = value; }//set method
+        }//end of property CanBook
+
+        public string ComputeSeatLabel()
+        {
+            return((char)(_row+64)).ToString()+_column.ToString();
+        }//end of ComputeSeatLabel
+
+    }//end of Seat class
+}//end of namespace
