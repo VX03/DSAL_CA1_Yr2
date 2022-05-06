8b9c03ff02fe1b8a399908944d7693edb477ae21
Vernell <vernell.lim2014@gmail.com>
2022-05-06 20:42:59 +0800

    Node and SeatDoubleLinkedList

---
+++ DSAL_CA1_Yr2/Classes/Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSAL_CA1_Yr2.Classes
+{
+    class Node
+    {
+        private Node _prev;
+        private Seat _seat;
+        private Node _next;
+
+        public Node(Seat pSeat)
+        {
+            _seat = pSeat;
+            _prev = null;
+            _next = null;   
+        }//End of constructor
+
+        public Seat Seat
+        {
+            get { return _seat; }
+            set { _seat = value; }
+        }//End of Seat
+
+        public Node Next
+        {
+            get { return _next; }
+            set { _next = value; }
+        }//End of Next
+
+        public Node Prev
+        {
+            get { return _prev; }
+            set { _prev = value; }
+        }//End of Previous
+
+    }//end of Node class
+}//end of namespace
---
+++ DSAL_CA1_Yr2/Classes/SeatDoubleLinkedList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSAL_CA1_Yr2.Classes
+{
+    class SeatDoubleLinkedList
+    {
+        //Make sure that "start" refers to first node of list
+
+        public Node Start { get; set; }//End of Start
+
+        public SeatDoubleLinkedList()
+        {
+            this.Start = null;
+        }//End of SeatDoubleLinkedList
+
+        public void InsertAtEnd(Seat pSeatData)
+        {
+            Node newNode = new Node(pSeatData);
+            if(this.Start == null)
+            {
+                this.Start = newNode;
+                return;
+            }//End if
+
+            Node p = this.Start;
+            //Traverse through the list until p refers to last node
+            while(p != null)
+            {
+                p = p.Next;
+            }//End of While
+            p.Next = newNode;
+            newNode.Prev = p;
+        }//End of InsertAtEnd
+
+        public Seat SearchByRowAndColumn(int pRow,int pColumn)
+        {
+            Node p = this.Start;
+            while(p != null)
+            {
+                if((p.Seat.Column == pColumn) && (p.Seat.Row == pRow))
+                {
+                    break;//Exit loop
+                }
+                p = p.Next;//Continue to next node
+            }
+
+            if(p == null)//Unable to find
+            {
+                return null;
+            }
+            else
+            {
+                return p.Seat;
+            }//end of if else
+
+        }//end of SearchByRowAndColumn
+
+    }//end of SeatDoubleLinkedList
+}//end of namespace
